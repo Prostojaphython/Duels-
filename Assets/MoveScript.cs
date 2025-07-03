a37c12fe9e0299e7dfbb8d48132e104810a66710
Prostojaphython <mihkahumka@gmail.com>
2025-07-03 14:00:49 +0300

    Guns
    Add Guns

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveScript : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float sprintSpeed = 8f;
+    public float jumpForce = 15f;
+
+    [Header("Mouse Look")]
+    public Transform cameraTransform;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 80f;
+
+    private Rigidbody rb;
+    private float xRotation = 0f;
+    public bool isGrounded;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+    [SerializeField] Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        GroundCheck();        // Проверка выведена в отдельный метод
+        HandleMouseLook();
+        HandleJump();
+
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeed = isSprinting ? sprintSpeed : moveSpeed;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 targetVelocity = move * currentSpeed;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = targetVelocity.x;
+        velocity.z = targetVelocity.z;
+        rb.velocity = velocity;
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            anim.SetBool("Jump", true);
+        }
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+        anim.SetBool("Run", rb.velocity.magnitude > 0.1f && isGrounded);
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Debug.Log("isGrounded = " + isGrounded);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+        anim.SetBool("Jump", false);
+    }
+}
---
+++ Assets/Pistol.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{   
+
+    void Start()
+    {
+        //задержки между выстрелами нет
+        
+        cooldown = 0;
+        auto = false;
+        ammoCurrent = 80;
+        ammoMax= 80;
+        ammoBackPack = 150;
+        //Стрельба не автоматическая. Нужно каждый раз нажимать на кнопку мыши для выстрела
+        
+    }
+
+    protected override void OnShoot()
+    {
+        Vector3 rayStartPosition = new Vector3(Screen.width / 2, Screen.height / 2, 0);        
+        Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(rayStartPosition);
+        RaycastHit hit;
+        //Продолжаем писать после строки RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject gameBullet = Instantiate(particle, hit.point, hit.transform.rotation);
+            //if(hit.collider.CompareTag("enemy"))
+            {
+                //Число 10 можешь поменять на своё. Это урон, который наносит одна пуля
+                //hit.collider.gameObject.GetComponent<Enemy>().ChangeHealth(10);
+            }
+            //Destroy(gameBullet, 1);
+        }
+
+    }
+}
